f35f612cdb77c3ddeeb7e20ace91a17a29747e87
Franco-byte <fd.reyes3110@gmail.com>
2024-12-13 17:56:54 +0000

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,24 +10,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i1, i2, tmp1, tmp2;
+	size_t i1, tmp;
+	int swap;
 
 	if (array == NULL || size < 2)
 		return;
+	do {
+		swap = 0;
 
-	for (i1 = 0; i1 < size; i1++)
+	for (i1 = 0; i1 < size - 1; i1++)
 	{
-		for (i2 = i1 + 1; i2 < size; i2++)
+		if (array[i1] > array[i1 + 1])
 		{
-			if (array[i1] > array[i2])
-			{
-				tmp1 = array[i1];
-				tmp2 = array[i2];
-				array[i1] = tmp2;
-				array[i2] = tmp1;
+			tmp = array[i1];
+			array[i1] = array[i1 + 1];
+			array[i1 + 1] = tmp;
 
-				print_array(array, size);
-			}
+			swap = 1;
+			print_array(array, size);
 		}
 	}
+	} while (swap);
 }
